fd140f7aa75de61770ee0dfc45f5adaacd897bb7
Adeayo Oladipo <adeayooladipo@gmail.com>
2025-03-08 23:33:05 +0000

    second commit

--- Program.cs
+++ Program.cs
@@ -1,32 +1,27 @@
-﻿/*******************************************************************************************
-*
-*   raylib [shapes] example - Draw basic shapes 2d (rectangle, circle, line...)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
-using System.Numerics;
-using Raylib_cs;
+﻿using Raylib_cs;
 using static Raylib_cs.Raylib;
 
 namespace Examples.Shapes;
 
 public class BasicShapes
 {
-    public static int Main()
+    public static void Main()
     {
         // Initialization
         //--------------------------------------------------------------------------------------
         const int screenWidth = 800;
         const int screenHeight = 450;
 
-        InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes drawing");
+        InitWindow(screenWidth, screenHeight, "Basic shape and image drawing");
 
-        SetTargetFPS(60);
+        //Load the image into the cpu
+        Image image = LoadImage("resources/raylib-cs_logo.png");
+
+        //Transform it as a texture
+        Texture2D texture = LoadTextureFromImage(image);
+
+        //Unload/release it
+        UnloadImage(image);
         //--------------------------------------------------------------------------------------
 
         // Main game loop
@@ -42,31 +37,28 @@ public class BasicShapes
             BeginDrawing();
             ClearBackground(Color.RayWhite);
 
-            DrawText("some basic shapes available on raylib", 20, 20, 20, Color.DarkGray);
+            /* Raylib supports drawing simple 2d shapes with internal functions 
+            so uncomment the following lines to see it in action */
 
-            DrawLine(18, 42, screenWidth - 18, 42, Color.Black);
+            // DrawText("some basic shapes available on raylib", 20, 20, 20, Color.DarkGray);
 
-            DrawCircle(screenWidth / 4, 120, 35, Color.DarkBlue);
-            DrawCircleGradient(screenWidth / 4, 220, 60, Color.Green, Color.SkyBlue);
-            DrawCircleLines(screenWidth / 4, 340, 80, Color.DarkBlue);
+            // DrawLine(18, 42, screenWidth - 18, 42, Color.Black);
 
-            DrawRectangle(screenWidth / 4 * 2 - 60, 100, 120, 60, Color.Red);
-            DrawRectangleGradientH(screenWidth / 4 * 2 - 90, 170, 180, 130, Color.Maroon, Color.Gold);
-            DrawRectangleLines(screenWidth / 4 * 2 - 40, 320, 80, 60, Color.Orange);
+            // DrawCircle(screenWidth / 4, 120, 35, Color.DarkBlue);
+            // DrawRectangle(screenWidth / 4 * 2 - 60, 100, 120, 60, Color.Red);
+            // DrawTriangle(
+            //     new Vector2(screenWidth / 4 * 3, 80),
+            //     new Vector2(screenWidth / 4 * 3 - 60, 150),
+            //     new Vector2(screenWidth / 4 * 3 + 60, 150), Color.Violet
+            // );
 
-            DrawTriangle(
-                new Vector2(screenWidth / 4 * 3, 80),
-                new Vector2(screenWidth / 4 * 3 - 60, 150),
-                new Vector2(screenWidth / 4 * 3 + 60, 150), Color.Violet
-            );
+            int x = screenWidth / 2 - texture.Width / 2;
+            int y = screenHeight / 2 - texture.Height / 2;
 
-            DrawTriangleLines(
-                new Vector2(screenWidth / 4 * 3, 160),
-                new Vector2(screenWidth / 4 * 3 - 20, 230),
-                new Vector2(screenWidth / 4 * 3 + 20, 230), Color.DarkBlue
-            );
+            //This is where we draw the texture
+            DrawTexture(texture, x - 200, y + 60, Color.White);
 
-            DrawPoly(new Vector2(screenWidth / 4 * 3, 320), 6, 80, 0, Color.Brown);
+
 
             EndDrawing();
             //----------------------------------------------------------------------------------
@@ -77,6 +69,5 @@ public class BasicShapes
         CloseWindow();
         //--------------------------------------------------------------------------------------
 
-        return 0;
     }
 }
